a05194bd5367a1cd08aa9304bba457d370d89649
anonimo anonimo <joaomachadoporto@hotmail.com>
2024-04-10 15:44:59 +0100

    end

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,47 +3,102 @@ import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display';
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0,
+}
+
 export default class Calculator extends Component {
+    state = { ...initialState }
+
     constructor(props) {
-        super(props); // Adiciona chamada ao super
+        super(props);
         this.clearMemory = this.clearMemory.bind(this);
         this.setOperation = this.setOperation.bind(this);
         this.addDigit = this.addDigit.bind(this);
     }
 
     clearMemory() {
-        console.log('clear'); 
+        this.setState({ ...initialState });
     }
 
     setOperation(operation) {
-        console.log(operation); 
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true });
+        } else {
+            const equals = operation === '=';
+            const currentOperation = this.state.operation;
+    
+            const values = [...this.state.values];
+            try {
+                // Avaliando o resultado do cálculo
+                let result = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+    
+                // Se o resultado é um número de ponto flutuante, arredonda apenas se necessário
+                if (!Number.isInteger(result)) {
+                    result = parseFloat(result.toFixed(8)); // Arredonda para até 8 casas decimais apenas se não for um inteiro
+                }
+    
+                values[0] = result;
+            } catch (e) {
+                values[0] = this.state.values[0];
+            }
+    
+            values[1] = 0;
+            this.setState({
+                displayValue: values[0].toString(),
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            });
+        }
     }
+    
+    
 
     addDigit(n) {
-        console.log(n); 
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return;
+        }
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+        const newDisplayValue = currentValue + n;
+        this.setState({ displayValue: newDisplayValue, clearDisplay: false });
+
+        if (n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(newDisplayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({ values });
+        }
     }
 
     render() {
         return (
             <div className='calculator'>
-                <Display value={100}/> 
-                <Button label="AC" click={() => this.clearMemory()}/>
-                <Button label="/" click={() => this.setOperation('/')}/>
+                <Display value={this.state.displayValue}/> 
+                <Button label="AC" click={this.clearMemory} triple/>
+                <Button label="/" click={() => this.setOperation('/')} operation/>
                 <Button label="7" click={() => this.addDigit('7')}/>
                 <Button label="8" click={() => this.addDigit('8')}/>
                 <Button label="9" click={() => this.addDigit('9')}/>
-                <Button label="*" click={() => this.setOperation('*')}/>
+                <Button label="*" click={() => this.setOperation('*')} operation/>
                 <Button label="4" click={() => this.addDigit('4')}/>
                 <Button label="5" click={() => this.addDigit('5')}/>
                 <Button label="6" click={() => this.addDigit('6')}/>
-                <Button label="-" click={() => this.setOperation('-')}/>
+                <Button label="-" click={() => this.setOperation('-')} operation/>
                 <Button label="1" click={() => this.addDigit('1')}/>
                 <Button label="2" click={() => this.addDigit('2')}/>
                 <Button label="3" click={() => this.addDigit('3')}/>
-                <Button label="+" click={() => this.setOperation('+')}/>
-                <Button label="0" click={() => this.addDigit('0')}/>
+                <Button label="+" click={() => this.setOperation('+')} operation/>
+                <Button label="0" click={() => this.addDigit('0')} double/>
                 <Button label="." click={() => this.addDigit('.')}/>
-                <Button label="=" click={() => this.setOperation('=')}/>
+                <Button label="=" click={() => this.setOperation('=')} operation/>
             </div>
         );
     }
